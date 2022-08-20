cdaad3400f42413b7a96d4e398fc3c6437d0170f
shruthi-22 <shruthi.palanisamy.20@gmail.com>
2022-08-20 20:31:57 +0530

    add test

--- src/features/transfer/__tests__/MoneyTransfer.test.js
+++ src/features/transfer/__tests__/MoneyTransfer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import {MoneyTransfer} from '../MoneyTransfer';
 
-it('renders Money transfer screen with default values', () => {
-  const {getAllByText} = render(<MoneyTransfer />);
+test('renders Money transfer screen with default values', () => {
+  render(<MoneyTransfer />);
   expect(getAllByText('Send Money').length).toBe(2);
 });
